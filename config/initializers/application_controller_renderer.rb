f7010f1a497bf4da70dd096c47d1cb67e1d68e51
kotahorii <khorii.micron@gmail.com>
2021-10-03 06:38:27 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6f68dd81d0389e3828c9ced6b7778ce2a226f44d350070c15bb0f25c005d990b0ac73ea0f90191d76e17270b967abcfd691976b04e94ce8a39e84cbd3706f586'
